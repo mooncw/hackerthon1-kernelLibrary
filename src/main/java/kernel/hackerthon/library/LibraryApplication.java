4713957b48c826b9746a6c8c80346c5c2823b361
김찬규 <kjkj5868@gmail.com>
2023-10-17 12:33:10 +0900

    start :: 프로젝트 생성

---
+++ src/main/java/kernel/hackerthon/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package kernel.hackerthon.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}

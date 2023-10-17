e22ca073199276fede51e577c19ae72c01563f89
Uknow928 <youknow928@yonsei.ac.kr>
2023-10-17 16:42:45 +0900

    conflict resolved

--- src/main/java/kernel/hackerthon/library/domain/Book.java
+++ src/main/java/kernel/hackerthon/library/domain/Book.java
@@ -16,17 +16,8 @@ public class Book {
 
     private Long isbn;
 
-<<<<<<< HEAD
     // false = 대출가능
     private Boolean rentalStatus;
-
-    //-- 비즈니스 로직 --//
-    public void changeRentalStatus(){
-        this.rentalStatus = !rentalStatus;
-    }
-=======
-    private boolean rentalStatus;
->>>>>>> da8ea1fea40ebb33f00ef1f23caae2c12a13a37e
 
     //-- 비즈니스 로직 --//
     // 대출 상태를 토글 하는 메서드 //

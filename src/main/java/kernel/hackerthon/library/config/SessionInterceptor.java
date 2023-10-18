202fd358fcf2d4d1bd47098b6d22687cd3efb596
이윤선 <yoonseon3@gmail.com>
2023-10-18 22:34:20 +0900

    feat: 로그인 구현

---
+++ src/main/java/kernel/hackerthon/library/config/SessionInterceptor.java
@@ -0,0 +1,48 @@
+package kernel.hackerthon.library.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SessionInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("request.getSession : "+request.getSession().getAttribute("loginUser"));
+        String[] allowedURLs = {
+                "/user/login",
+                "/user/loginProcess",
+                "/user/join",
+                "/books",
+                "/user/logout"
+
+        };  // 세션 체크하지 않을 url 명시
+
+        String requestURI = request.getRequestURI();
+
+        for (String allowedURL : allowedURLs) {
+            if (requestURI.endsWith(allowedURL)) {
+                return true;  // 세션 체크를 하지 않음
+            }
+        }
+
+        if (request.getSession().getAttribute("loginUser") == null) {
+            response.sendRedirect(request.getContextPath() + "/user/login");
+            return false;  // 컨트롤러 실행을 중단
+        }
+        return true;  // 컨트롤러 실행 계속
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+
+    }
+}
--- src/main/java/kernel/hackerthon/library/repository/UserRepository.java
+++ src/main/java/kernel/hackerthon/library/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package kernel.hackerthon.library.repository;
 
 
-import kernel.hackerthon.library.domain.Book;
 import kernel.hackerthon.library.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends
-        JpaRepository<User, Long>
-{}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
 

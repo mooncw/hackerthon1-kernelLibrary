1b66db99f9af41d7b3db8cee86fbb1eaae8a3b49
김찬규 <kjkj5868@gmail.com>
2023-10-19 10:42:28 +0900

    feat :: 도서에 사용자 연관매핑

--- src/main/java/kernel/hackerthon/library/domain/Book.java
+++ src/main/java/kernel/hackerthon/library/domain/Book.java
@@ -26,6 +26,10 @@ public class Book {
     @Column(name="is_recovery", columnDefinition = "varchar", length = 255)
     private Boolean isRecovery;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Builder
     public Book(Long id, String name, Boolean isRental, Boolean isRecovery) {
         this.id = id;

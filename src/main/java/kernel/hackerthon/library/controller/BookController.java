35af735cfc72b9a3588bad989d0ebf1bdb1fd0f4
김찬규 <kjkj5868@gmail.com>
2023-10-20 13:44:07 +0900

    refactor :: 불필요한 주석, 메서드 삭제. 모호한 메서드명 변경

--- src/main/java/kernel/hackerthon/library/controller/BookController.java
+++ src/main/java/kernel/hackerthon/library/controller/BookController.java
@@ -27,36 +27,21 @@ public class BookController {
 
     private String apiKey;
 
-    // (메인 서고화면)서고에 있는 모든 책 get 하기
-    // 회수한 책은 제외하도록 처리해야
     @GetMapping
-    public String books(Model model) {
+    public String findBooksAvailable(Model model) {
         List<Book> books = bookService.findBooksByIsRecoveryIsFalse();
         model.addAttribute("books", books);
         return "firstpage";
     }
 
 
-    //(메인 화면에서 책 한권 클릭시)책 한권 get - 책 상세페이지
     @GetMapping("/{bookId}")
-    public String book(@PathVariable Long bookId, ModelMap map) {
+    public String findBookByBookId(@PathVariable Long bookId, ModelMap map) {
         Optional<Book> book = bookService.getBook(bookId);
 
         map.addAttribute("book", book);
-        // view에서 book 의 대출상태가 대출가능 이어야 대출 버튼 보이게 처리
+
         return "books/detail";
-    }
-
-    // 책 한권 페이지 - 책 상세페이지에서 [빌리기] 눌렀을때
-    @PostMapping("/{bookId}/borrow/{userId}")
-    public String borrowBook(
-            @PathVariable Long bookId,
-            @PathVariable Long userId
-    )
-    {
-
-
-        return "redirect:/books";
     }
 
     @GetMapping("/add")
